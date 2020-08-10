d2f406a70f8c732d6131a8bf8d76afc73273e7cd
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2020-08-10 16:49:03 +0900

    cleanup env vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
   production: false,
   midgardUrl: 'https://midgard.bepswap.com',
   thorNodeUrl: 'http://18.158.69.134:1317'
-  // thorNodeUrl: 'https://ui1pjvknb0.execute-api.us-east-1.amazonaws.com/default'
 };
 
 /*
