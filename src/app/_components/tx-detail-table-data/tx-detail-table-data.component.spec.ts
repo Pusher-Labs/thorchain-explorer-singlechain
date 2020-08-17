d66367811d345e20a1c444177f52a6c57d2acb18
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2020-08-17 15:39:38 +0900

    tests passing

--- src/app/_components/tx-detail-table-data/tx-detail-table-data.component.spec.ts
+++ src/app/_components/tx-detail-table-data/tx-detail-table-data.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { TxDetailTableDataComponent } from './tx-detail-table-data.component';
 
 describe('TxDetailTableDataComponent', () => {
@@ -8,7 +8,8 @@ describe('TxDetailTableDataComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TxDetailTableDataComponent ]
+      declarations: [ TxDetailTableDataComponent ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
   }));
