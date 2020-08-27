ee7f2270e060ed2df423cf2d54ee6ae9692a8359
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2020-08-27 14:42:03 +0900

    fixes logo for asset pool page

--- src/app/pools/pools.component.ts
+++ src/app/pools/pools.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PoolService } from '../_services/pool.service';
 
 @Component({
