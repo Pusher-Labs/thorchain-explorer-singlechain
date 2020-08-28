a1abffb132894f99185fe5f44f8ff5cccf1b0381
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2020-08-28 19:56:25 +0900

    pass tests

--- src/app/network/constants/constants.component.spec.ts
+++ src/app/network/constants/constants.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ConstantsComponent } from './constants.component';
 
 describe('ConstantsComponent', () => {
@@ -8,7 +8,8 @@ describe('ConstantsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConstantsComponent ]
+      declarations: [ ConstantsComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   }));
