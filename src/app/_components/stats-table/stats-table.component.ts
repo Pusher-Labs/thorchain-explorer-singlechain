05fbd2c2b5a980b41842c72b77ec7b8646d643c4
unknown <gribmail@gmail.com>
2020-09-07 13:05:04 +0300

    added dashboard stats with usd value

--- src/app/_components/stats-table/stats-table.component.ts
+++ src/app/_components/stats-table/stats-table.component.ts
@@ -1,15 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { GlobalStats } from 'src/app/_services/stats.service';
+import { CoinGeckoService } from 'src/app/_services/coingecko.service';
 
 @Component({
   selector: 'app-stats-table',
   templateUrl: './stats-table.component.html',
   styleUrls: ['./stats-table.component.scss']
 })
-export class StatsTableComponent {
+export class StatsTableComponent implements OnInit {
 
   @Input() stats: GlobalStats;
+  currentRate: number;
+  volume24hr: number;
+  totalVolume: number;
+  totalStaked: number;
+  totalDepth: number;
 
-  constructor() { }
+  constructor(private coinGeckoService: CoinGeckoService) { }
+
+  ngOnInit(): void {
+    this.getCurrentRate();
+  }
+
+  getCurrentRate() {
+    this.coinGeckoService.getCurrencyConversion().subscribe(
+      (res) => {
+        if (res.thorchain && res.thorchain.usd) {
+          this.currentRate = res.thorchain.usd;
+          this.getStatsValue();
+        }
+      },
+      (err) => console.error('error fetching current thorchain rate: ', err)
+    );
+  }
+
+  getStatsValue(): void {
+    this.volume24hr = parseInt(this.stats.totalVolume24hr, 10) / 10 ** 8;
+    this.totalVolume = parseInt(this.stats.totalVolume, 10) / 10 ** 8;
+    this.totalStaked = parseInt(this.stats.totalStaked, 10) / 10 ** 8;
+    this.totalDepth = parseInt(this.stats.totalDepth, 10) / 10 ** 8;
+  }
 
 }
