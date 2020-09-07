5796f3b5cccd1f7793636c7a4b0f605bea50c389
Kashif <static.kash@gmail.com>
2020-09-07 16:46:41 +0900

    add table highlights

--- src/app/_components/nodes-table/nodes-table.component.ts
+++ src/app/_components/nodes-table/nodes-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ThorNode } from 'src/app/_classes/thor-node';
+import { IconDefinition, faThumbtack } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nodes-table',
@@ -11,9 +12,33 @@ export class NodesTableComponent implements OnInit {
   @Input() nodes: ThorNode[];
   @Input() tableName: string;
 
-  constructor() { }
+  pinIcon: IconDefinition;
+
+  constructor() {
+    this.pinIcon = faThumbtack;
+  }
 
   ngOnInit(): void {
+  }
+
+  isPinned(nodeAddress: string){
+    let pinned = false;
+    try{
+      const result = localStorage.getItem(`pinned-${nodeAddress}`);
+      if (result !== null){
+        pinned = JSON.parse(result);
+      }
+    }catch (err){}
+    return !!pinned;
+  }
+
+  toggleNodePin(nodeAddress: string){
+    let currentValue = false;
+    try{
+      currentValue = JSON.parse(localStorage.getItem(`pinned-${nodeAddress}`));
+    }catch (err){}
+
+    localStorage.setItem(`pinned-${nodeAddress}`, JSON.stringify(!currentValue));
   }
 
 }
