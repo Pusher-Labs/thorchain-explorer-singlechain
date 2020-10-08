fb5af2d9112aa117202e7b39c0b5c67d016cfd39
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2020-10-08 17:26:12 +0900

    future notes for updates

--- src/app/_classes/pool-member.ts
+++ src/app/_classes/pool-member.ts
@@ -2,8 +2,8 @@ export interface PoolMember {
     asset: string;
     rune_address: string;
     asset_address: string;
-    last_stake: string;
-    last_unstake: string;
+    last_stake: string; // will change to last_deposit
+    last_unstake: string; // will change to last_withdraw
     units: string;
     pending_rune: string;
     pending_tx_id: string;
